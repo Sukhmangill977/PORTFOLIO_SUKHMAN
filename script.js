8fae6e17e1aea9215c4aba5f45b692280e272af8
Sukhmangill977 <sukhmangill977@gmail.com>
2024-10-13 15:44:55 +0530

    updated

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const logo = document.getElementById('logo');
+const logoMobile = document.getElementById('logo-mobile');
+const colors = ['#ff5733', '#33c9ff', '#a833ff', '#ff33a6']; // Array of colors
+let colorIndex = 0;
+
+function changeLogoColor() {
+  logo.style.color = colors[colorIndex];
+  logoMobile.style.color = colors[colorIndex];
+  colorIndex = (colorIndex + 1) % colors.length; // Cycle through colors
+}
+
+setInterval(changeLogoColor, 2000); // Change color every 5 seconds
